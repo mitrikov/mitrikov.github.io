945ae67b35055356f0eabb50a87e0996b427e9e0
Evgen <j6p@bk.ru>
2016-03-08 13:36:40 +1200

    Вторая версия проекта

--- dynamicData.js
+++ dynamicData.js
@@ -2,7 +2,4 @@ window.onload = function() {
 var	d = document,
 	date = new Date();
 	d.getElementById("year").innerHTML = date.getFullYear();
-	if(d.getElementById("domain")) {
-		d.getElementById("domain").innerHTML = window.location.host; 
-	}
 }
