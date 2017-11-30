2159fc73a96837f238941d9b24e2022915495d73
vokirim <j6p@bk.ru>
2017-11-30 21:21:01 +1200

    Update dynamicData.js

--- dynamicData.js
+++ dynamicData.js
@@ -1,5 +1,9 @@
 window.onload = function() {
-var	d = document,
-	date = new Date();
-	d.getElementById("year").innerHTML = date.getFullYear();
+	var	d = document,
+		date = new Date();
+		d.getElementById("year").innerHTML = date.getFullYear();
+	const bgLeft = new Image();
+	const bgRight = new Image();
+	bgLeft.src = "images/button_bg_left.jpg";
+	bgRight.src = "images/button_bg_right.jpg";
 }
